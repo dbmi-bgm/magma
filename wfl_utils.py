058b41b34abba7b25f8d391e0589c9d58362fb56
Michele Berselli <berse@MacBook-Pro.local>
2021-04-27 11:54:54 -0400

    init

---
+++ wfl_utils.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python3
+
+################################################
+#
+#   Library to parse meta-workflow objects
+#
+#   Michele Berselli
+#   berselli.michele@gmail.com
+#
+################################################
+
+################################################
+#   Libraries
+################################################
+import sys, argparse, os
+import json
+import copy
+
+################################################
+#   Objects
+################################################
+class Wfl(object):
+    '''
+        object to represent a meta-workflow
+    '''
+
+    def __init__(self, wfl_json):
+        '''
+            initialize Wfl object from wfl_json
+
+                wfl_json is a meta-workflow in json format
+        '''
+        # Basic attributes
+        try:
+            self.accession = wfl_json['accession'] #str, need to be unique
+            self.app_name = wfl_json['app_name'] #str, need to be unique
+            self.app_version = wfl_json['app_version'] #str
+            self.uuid = wfl_json['uuid'] #str, need to be unique
+            self.arguments = wfl_json['arguments'] #dict
+            self.workflows = wfl_json['workflows'] #dict
+        except KeyError as e:
+            raise ValueError('Validation error, missing key {0} in meta-workflow json\n{1}\n'
+                                .format(e.args[0], wfl_json))
+        #end try
+        # Calculated attributes
+        self.steps = {} #{step_obj.name: step_obj, ...}
+
+        # Calculate attributes
+        self._read_steps()
+    #end def
+
+    class Step(object):
+        '''
+            object to represent a meta-workflow step
+        '''
+
+        def __init__(self, step_json):
+            '''
+                initialize Step object
+
+                    step_json is a step workflow in json format
+            '''
+            # Basic attributes
+            try:
+                self.name = step_json['name'] #str, need to be unique
+                self.uuid = step_json['uuid'] #str, need to be unique
+                self.config = step_json['config'] #dict
+                self.arguments = step_json['arguments'] #dict
+                self.outputs = step_json['outputs'] #dict
+            except KeyError as e:
+                raise ValueError('Validation error, missing key {0} in step workflow json\n{1}\n'
+                                    .format(e.args[0], step_json))
+            #end try
+            # Calculated attributes
+            self.is_scatter = False
+            self.gather_from = set() #names of steps to gather from
+            self.dependencies = set() #names of steps that are dependency
+            # For building graph structure
+            self._nodes = set() #step_objects for steps that depend on current step
+
+            # Calculate attributes
+            self._attributes()
+        #end def
+
+        def _attributes(self):
+            '''
+            '''
+            for arg in self.arguments:
+                if not self.is_scatter and arg.get('scatter'):
+                    self.is_scatter = True
+                #end if
+                source_step = arg.get('source_step') #source step name
+                if source_step:
+                    self.dependencies.add(source_step)
+                    if arg.get('gather'):
+                        self.gather_from.add(source_step)
+                    #end if
+                #end if
+            #end for
+        #end def
+
+    #end class
+
+    def _read_steps(self):
+        '''
+        '''
+        for wfl in self.workflows:
+            step_obj = self.Step(wfl)
+            if step_obj.name not in self.steps:
+                self.steps.setdefault(step_obj.name, step_obj)
+            else:
+                raise ValueError('Validation error, step {0} duplicate in step workflows\n'
+                                    .format(step_obj.name))
+            #end if
+        #end for
+    #end def
+
+    def _build_wfl_run(self, end_steps):
+        '''
+
+            end_steps, names list of end steps to build wfl_run for
+        '''
+        steps_ = set() #steps that are entry point to wfl_run
+        for end_step in end_steps:
+            # Initialize queue with end_step
+            queue = [self.steps[end_step]]
+            # Reconstructing dependencies
+            while queue:
+                step_obj = queue.pop(0)
+                if step_obj.dependencies:
+                    for dependency in step_obj.dependencies:
+                        try:
+                            queue.append(self.steps[dependency])
+                            self.steps[dependency]._nodes.add(step_obj)
+                        except Exception:
+                            raise ValueError('Validation error, missing dependency step {0} in step workflows\n'
+                                                .format(dependency))
+                        #end try
+                    #end for
+                else: steps_.add(step_obj)
+                #end if
+            #end while
+        #end for
+        return steps_
+    #end def
+
+    def _order_wfl_run(self, end_steps):
+        '''
+
+            end_steps, names list of end steps to build wfl_run for
+        '''
+        steps_ = []
+        queue = list(self._build_wfl_run(end_steps))
+        while queue:
+            step_obj = queue.pop(0)
+            # Adding next steps to queue
+            for node in step_obj._nodes:
+                if node not in steps_ and node not in queue:
+                    queue.append(node)
+                #end if
+            #end for
+            # Checking if dependencies are satisfied already and step can be added to steps_
+            is_dependencies = True
+            if step_obj.dependencies:
+                for dependency in step_obj.dependencies:
+                    if self.steps[dependency] not in steps_:
+                        is_dependencies = False
+                        queue.append(step_obj)
+                        break
+                    #end if
+                #end for
+            #end if
+            if is_dependencies:
+                steps_.append(step_obj)
+            #end if
+        #end while
+        return steps_
+    #end def
+
+    def write_wfl_run(self, end_steps, input=3):
+        '''
+
+            end_steps, names list of end steps to build wfl_run for
+        '''
+        n = input
+        scatter = {}
+        steps_ = self._order_wfl_run(end_steps)
+        run_json = {
+            'meta_workflow_uuid': self.uuid,
+            'workflow_runs': [],
+            'input': []
+        }
+        for step_obj in steps_:
+            run_step = {}
+            run_step.setdefault('name', step_obj.name)
+            # run_step.setdefault('workflow_run_uuid', '')
+            # run_step.setdefault('output', '')
+            # run_step.setdefault('status', 'pending')
+            run_step.setdefault('dependencies', [])
+            # Check scatter
+            #   If dependency in scatter but not in gather_from,
+            #       current step must be scattered.
+            scatter_ = 1 #default is no scatter, 1 step
+            if step_obj.is_scatter:
+                scatter_ = n
+                scatter.setdefault(step_obj.name, scatter_)
+            else:
+                for dependency in step_obj.dependencies:
+                    if dependency in scatter:
+                        if dependency not in step_obj.gather_from:
+                            scatter_ = scatter[dependency]
+                            scatter.setdefault(step_obj.name, scatter_)
+                            break
+                        #end if
+                    #end if
+                #end for
+            #end if
+            # Created shards
+            for i in range(scatter_):
+                run_step_ = copy.deepcopy(run_step)
+                run_step_.setdefault('shard', i)
+                # Check gather_from
+                #   If dependency in gather_from,
+                #       dependencies must be aggregated from scatter.
+                for dependency in step_obj.dependencies:
+                    if dependency in step_obj.gather_from:
+                        for i_ in range(scatter[dependency]):
+                            run_step_['dependencies'].append('{0}:{1}'.format(dependency, i_))
+                        #end for
+                    else:
+                        run_step_['dependencies'].append('{0}:{1}'.format(dependency, i))
+                    #end if
+                #end for
+                run_json['workflow_runs'].append(run_step_)
+            #end for
+        #end for
+        return run_json
+    #end def
+
+#end class
