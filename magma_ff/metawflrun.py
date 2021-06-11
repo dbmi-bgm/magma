f391e4bbd4faa7085135110f8f435213bed16cd8
Michele Berselli <berse@MacBook-Pro.local>
2021-06-11 13:33:13 -0400

    .

--- magma_ff/metawflrun.py
+++ magma_ff/metawflrun.py
@@ -1,11 +1,11 @@
 import copy
 from magma.metawflrun import MetaWorkflowRun as MetaWorkflowRunFromMagma
-from .parser_ff import ParserFF
+from magma_ff.parser_ff import ParserFF
 
 
 class MetaWorkflowRun(MetaWorkflowRunFromMagma):
     def __init__(self, input_json):
-        input_json_copy = copy.deepcopy(input_json)
-        ParserFF(input_json_copy).arguments_to_json()
+        input_json_ = copy.deepcopy(input_json)
+        ParserFF(input_json_).arguments_to_json()
 
-        super().__init__(input_json_copy)
+        super().__init__(input_json_)
