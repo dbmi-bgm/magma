047c52df1ad7d7ae11a22b89c2986704450287ab
SooLee <duplexa@gmail.com>
2021-06-24 12:35:46 -0400

    create_metawfr_v22 -> create_metawfr, the function takes an additional parameter type

--- magma_ff/create_metawfr.py
+++ magma_ff/create_metawfr.py
@@ -5,30 +5,92 @@ from magma_ff.metawfl import MetaWorkflow
 from magma_ff.metawflrun import MetaWorkflowRun
 from dcicutils import ff_utils
 
-metawf_uuid = '475a0518-9b72-4dc1-b049-070f366ab821'  # v22
 
-# ff_key = ff_utils.get_authentication_with_server(ff_env='fourfront-cgapwolf')
-# case_uuid = '1ea344bc-e211-4b11-8bde-7b94dadda7e2'  # NA12879 on cgapwolf
-
-ff_key = ff_utils.get_authentication_with_server(ff_env='fourfront-cgap')
-case_uuid = '81278096-7c10-4c5a-8ac3-5e3014e83bbb'  # NA12879 on cgap
-
-
-def create_metawfr_from_case_proband_only(metawf_uuid, case_uuid, ff_key, post=False, verbose=False):
-
-    def sort_pedigree(pedigree):
-        sorted_pedigree = sorted(pedigree, key=lambda x: x['relationship'] != 'proband') # make it proband-first
-        sorted_pedigree[1:] = sorted(sorted_pedigree[1:], key=lambda x: x['relationship'] not in ['mother','father']) # parents next
-        return sorted_pedigree
-
+def create_metawfr_from_case(metawf_uuid, case_uuid, type, ff_key, post=False, verbose=False):
+    """This is the main API - the rest are internal functions.
+    type should be 'WGS trio', 'WGS proband', 'WGS cram proband'
+    """
     case_meta = ff_utils.get_metadata(case_uuid, add_on='?frame=raw', key=ff_key)
     sp_uuid = case_meta['sample_processing']
     sp_meta = ff_utils.get_metadata(sp_uuid, add_on='?frame=object', key=ff_key)
     pedigree = sp_meta['samples_pedigree']
     pedigree = remove_parents_without_sample(pedigree)  # remove no-sample individuals
     pedigree = sort_pedigree(pedigree)
-    pedigree = pedigree[0:1]
 
+    if type == 'WGS cram proband':
+        pedigree = pedigree[0:1]
+        input = create_metawfr_input_from_pedigree_proband_only(pedigree)
+    elif type == 'WGS proband':
+        pedigree = pedigree[0:1]
+        input = create_metawfr_input_from_pedigree_proband_only(pedigree)
+    elif type == 'WGS trio':
+        input = create_metawfr_input_from_pedigree_trio(pedigree)
+
+    metawfr = create_metawfr_from_input(input, metawf_uuid, case_meta)
+
+    # post meta-wfr
+    if post:
+        res_post = ff_utils.post_metadata(metawfr, 'MetaWorkflowRun', key=ff_key)
+        if verbose:
+            print(res_post)
+
+    return metawfr
+
+
+def create_metawfr_from_input(metawfr_input, metawf_uuid, case_meta):
+    metawf_meta = ff_utils.get_metadata(metawf_uuid, add_on='?frame=raw', key=ff_key)
+    metawfr = {'meta_workflow': metawf_uuid,
+               'input': metawfr_input,
+               'title': 'MetaWorkflowRun %s on case %s' % (metawf_meta['title'], case_meta['accession']),
+               #'name': 'metawf_%s_on_case_%s' % (metawf_meta['accession'], case_meta['accession']),
+               #'name': 'MetaWorkflowRun %s for case %s' % (metawf_meta['title'], case_meta['accession']),
+               'project': case_meta['project'],
+               'institution': case_meta['institution'],
+               'common_fields': {'project': case_meta['project'],
+                                 'institution': case_meta['institution']},
+               'final_status': 'pending',
+               'workflow_runs' : [],
+               'uuid': str(uuid.uuid4())}
+
+    mwf = MetaWorkflow(metawf_meta)
+
+    # get the input structure in magma format
+    metawfr_mgm = MetaWorkflowRun(metawfr).to_json()
+    input_structure = metawfr_mgm['input'][0]['files']
+
+    # create workflow_runs
+    mwfr = mwf.write_run(input_structure)
+    metawfr['workflow_runs'] = mwfr['workflow_runs']
+
+    return metawfr
+
+
+def create_metawfr_input_from_pedigree_cram_proband_only(pedigree):
+    sample = pedigree[0]
+    sample_acc = sample['sample_accession']
+    sample_meta = ff_utils.get_metadata(sample_acc, add_on='?frame=raw', key=ff_key)
+    cram_uuids = sample_meta['cram_files']
+    cram_files = [{'file': cf, 'dimension': str(i)} for i, cf in enumerate(cram_uuids)]
+
+    # sample names
+    sample_names = [sample['sample_name']]
+    sample_names_str = json.dumps(sample_names)
+
+    # qc pedigree parameter
+    qc_pedigree_str = json.dumps(pedigree_to_qc_pedigree(pedigree))
+
+    # family size
+    family_size = len(pedigree)
+
+    # create metawfr input
+    input = [{'argument_name': 'crams', 'argument_type': 'file', 'files': cram_files},
+             {'argument_name': 'sample_names', 'argument_type': 'parameter', 'value': sample_names_str, 'value_type': 'json'},
+             {'argument_name': 'pedigree', 'argument_type': 'parameter', 'value': qc_pedigree_str, 'value_type': 'string'}]
+
+    return input
+
+
+def create_metawfr_input_from_pedigree_proband_only(pedigree):
     sample = pedigree[0]
     sample_acc = sample['sample_accession']
     sample_meta = ff_utils.get_metadata(sample_acc, add_on='?frame=raw', key=ff_key)
@@ -62,60 +124,15 @@ def create_metawfr_from_case_proband_only(metawf_uuid, case_uuid, ff_key, post=F
     family_size = len(pedigree)
 
     # create metawfr
-    metawf_meta = ff_utils.get_metadata(metawf_uuid, add_on='?frame=raw', key=ff_key)
-
     input = [{'argument_name': 'fastqs_R1', 'argument_type': 'file', 'files': r1_uuids},
              {'argument_name': 'fastqs_R2', 'argument_type': 'file', 'files': r2_uuids},
              {'argument_name': 'sample_names', 'argument_type': 'parameter', 'value': sample_names_str, 'value_type': 'json'},
              {'argument_name': 'pedigree', 'argument_type': 'parameter', 'value': qc_pedigree_str, 'value_type': 'string'}]
 
-    metawfr = {'meta_workflow': metawf_uuid,
-               'input': input,
-               'title': 'MetaWorkflowRun %s on case %s' % (metawf_meta['title'], case_meta['accession']),
-               #'name': 'metawf_%s_on_case_%s' % (metawf_meta['accession'], case_meta['accession']),
-               #'name': 'MetaWorkflowRun %s for case %s' % (metawf_meta['title'], case_meta['accession']),
-               'project': case_meta['project'],
-               'institution': case_meta['institution'],
-               'common_fields': {'project': case_meta['project'],
-                                 'institution': case_meta['institution']},
-               'final_status': 'pending',
-               'workflow_runs' : [],
-               'uuid': str(uuid.uuid4())}
-
-    mwf = MetaWorkflow(metawf_meta)
-
-    # get the input structure in magma format
-    metawfr_mgm = MetaWorkflowRun(metawfr).to_json()
-    input_structure = metawfr_mgm['input'][0]['files']
-
-    # create workflow_runs
-    mwfr = mwf.write_run(input_structure)
-    metawfr['workflow_runs'] = mwfr['workflow_runs']
-
-    # post meta-wfr
-    if post:
-        res_post = ff_utils.post_metadata(metawfr, 'MetaWorkflowRun', key=ff_key)
-        if verbose:
-            print(res_post)
-
-    return metawfr
+    return input
 
 
-
-def create_metawfr_from_case(metawf_uuid, case_uuid, ff_key, post=False, verbose=False):
-
-    def sort_pedigree(pedigree):
-        sorted_pedigree = sorted(pedigree, key=lambda x: x['relationship'] != 'proband') # make it proband-first
-        sorted_pedigree[1:] = sorted(sorted_pedigree[1:], key=lambda x: x['relationship'] not in ['mother','father']) # parents next
-        return sorted_pedigree
-
-    case_meta = ff_utils.get_metadata(case_uuid, add_on='?frame=raw', key=ff_key)
-    sp_uuid = case_meta['sample_processing']
-    sp_meta = ff_utils.get_metadata(sp_uuid, add_on='?frame=object', key=ff_key)
-    pedigree = sp_meta['samples_pedigree']
-    pedigree = remove_parents_without_sample(pedigree)  # remove no-sample individuals
-    pedigree = sort_pedigree(pedigree)
-
+def create_metawfr_input_from_pedigree_trio(pedigree):
     # prepare fastq input
     r1_uuids_fam = []
     r2_uuids_fam = []
@@ -153,8 +170,6 @@ def create_metawfr_from_case(metawf_uuid, case_uuid, ff_key, post=False, verbose
     rcktar_content_file_names_str = json.dumps(rcktar_content_file_names)
 
     # create metawfr
-    metawf_meta = ff_utils.get_metadata(metawf_uuid, add_on='?frame=raw', key=ff_key)
-
     input = [{'argument_name': 'fastqs_proband_first_R1', 'argument_type': 'file', 'files': r1_uuids_fam},
              {'argument_name': 'fastqs_proband_first_R2', 'argument_type': 'file', 'files': r2_uuids_fam},
              {'argument_name': 'sample_names_proband_first', 'argument_type': 'parameter', 'value': sample_names_str, 'value_type': 'json'},
@@ -162,36 +177,13 @@ def create_metawfr_from_case(metawf_uuid, case_uuid, ff_key, post=False, verbose
              {'argument_name': 'family_size', 'argument_type': 'parameter', 'value': str(family_size), 'value_type': 'integer'},
              {'argument_name': 'rcktar_content_file_names', 'argument_type': 'parameter', 'value': rcktar_content_file_names_str, 'value_type': 'json'}]
 
-    metawfr = {'meta_workflow': metawf_uuid,
-               'input': input,
-               'title': 'MetaWorkflowRun %s on case %s' % (metawf_meta['title'], case_meta['accession']),
-               #'name': 'metawf_%s_on_case_%s' % (metawf_meta['accession'], case_meta['accession']),
-               #'name': 'MetaWorkflowRun %s for case %s' % (metawf_meta['title'], case_meta['accession']),
-               'project': case_meta['project'],
-               'institution': case_meta['institution'],
-               'common_fields': {'project': case_meta['project'],
-                                 'institution': case_meta['institution']},
-               'final_status': 'pending',
-               'workflow_runs' : [],
-               'uuid': str(uuid.uuid4())}
+    return input
 
-    mwf = MetaWorkflow(metawf_meta)
 
-    # get the input structure in magma format
-    metawfr_mgm = MetaWorkflowRun(metawfr).to_json()
-    input_structure = metawfr_mgm['input'][0]['files']
-
-    # create workflow_runs
-    mwfr = mwf.write_run(input_structure)
-    metawfr['workflow_runs'] = mwfr['workflow_runs']
-
-    # post meta-wfr
-    if post:
-        res_post = ff_utils.post_metadata(metawfr, 'MetaWorkflowRun', key=ff_key)
-        if verbose:
-            print(res_post)
-
-    return metawfr
+def sort_pedigree(pedigree):
+    sorted_pedigree = sorted(pedigree, key=lambda x: x['relationship'] != 'proband') # make it proband-first
+    sorted_pedigree[1:] = sorted(sorted_pedigree[1:], key=lambda x: x['relationship'] not in ['mother','father']) # parents next
+    return sorted_pedigree
 
 
 def pedigree_to_qc_pedigree(samples_pedigree):
@@ -210,6 +202,7 @@ def pedigree_to_qc_pedigree(samples_pedigree):
             }
         qc_pedigree.append(member_qc_pedigree)
     return qc_pedigree
+
 
 def remove_parents_without_sample(samples_pedigree):
     individuals = [i['individual'] for i in samples_pedigree]
