64f0bd8ea8a2964358622fd7874efe934cfb71a9
SooLee <duplexa@gmail.com>
2021-06-24 13:30:37 -0400

    run_metawfr metawfr_uuid no longer needed

--- magma_ff/run_metawfr.py
+++ magma_ff/run_metawfr.py
@@ -27,20 +27,20 @@ from tibanna.core import API
 ################################################
 #   run_metawfr
 ################################################
-def run_metawfr(metawf_uuid, metawfr_uuid, ff_key, verbose=False, sfn='tibanna_zebra', env='fourfront-cgap'):
+def run_metawfr(metawfr_uuid, ff_key, verbose=False, sfn='tibanna_zebra', env='fourfront-cgap'):
     """
-            metawf_uuid, uuid for meta-workflow
             metawfr_uuid, uuid for meta-workflow-run to run
     """
-    # Get meta-workflow json from the portal
-    wfl_json = ff_utils.get_metadata(metawf_uuid, add_on='?frame=raw', key=ff_key)
-    # Create MetaWorkflow object for meta-workflow
-    wfl_obj = MetaWorkflow(wfl_json)
-
     # Get meta-workflow-run json from the portal
     run_json = ff_utils.get_metadata(metawfr_uuid, add_on='?frame=raw', key=ff_key)
     # Create MetaWorkflowRun object for meta-workflow-run
     run_obj = MetaWorkflowRun(run_json)
+
+    # Get meta-workflow json from the portal
+    metawf_uuid = run_json['meta_workflow']
+    wfl_json = ff_utils.get_metadata(metawf_uuid, add_on='?frame=raw', key=ff_key)
+    # Create MetaWorkflow object for meta-workflow
+    wfl_obj = MetaWorkflow(wfl_json)
 
     # Create InputGenerator object
     ingen_obj = ingen.InputGenerator(wfl_obj, run_obj)
