24a07c5dbe1e2c3b9355d4e7a51608e172509d9a
Michele Berselli <berse@MacBook-Pro.local>
2021-06-25 16:27:44 -0400

    .

--- magma_ff/metawflrun.py
+++ magma_ff/metawflrun.py
@@ -30,4 +30,22 @@ class MetaWorkflowRun(MetaWorkflowRunFromMagma):
         super().__init__(input_json_)
     #end def
 
+    def _reset_run(self, shard_name):
+        """
+            reset attributes value for WorkflowRun object in runs
+
+                shard_name, is the name of the workflow-run to reset
+        """
+        run_obj = self.runs[shard_name]
+        # Reset run_obj
+        run_obj.output = []
+        run_obj.status = 'pending'
+        if getattr(run_obj, 'jobid', None):
+            delattr(run_obj, 'jobid')
+        #end if
+        if getattr(run_obj, 'workflow_run', None):
+            delattr(run_obj, 'workflow_run')
+        #end if
+    #end def
+
 #end class
