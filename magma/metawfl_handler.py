165a612929426c83999ff5d3311044e6d5270e2f
vstevensf <victoria_stevens@hms.harvard.edu>
2022-10-24 13:10:41 -0400

    Further editing of baseline MWF handler, with added functions for calculated attributes

--- magma/metawfl_handler.py
+++ magma/metawfl_handler.py
@@ -3,14 +3,16 @@
 ################################################
 #   Libraries
 ################################################
-import copy
-from metawfl import MetaWorkflow
+# from magma import metawfl #TODO: do this in FF
+
+################################################
+#   TODO: functions for dcic utils -- move later
+################################################
+from magma.utils import check_presence_of_attributes, set_list_attributes, generate_ordered_steps_list
 
 ################################################
 #   MetaWorkflowStep
 ################################################
-#TODO: should i put this within metaworkflowhandler class? for ease of imports is the only
-# reason I can think of as reasoning for doing so
 
 class MetaWorkflowStep(object):
     """
@@ -22,7 +24,7 @@ class MetaWorkflowStep(object):
         """
         Constructor method, initialize object and attributes.
 
-        :param input_dict: a MetaWorkflow step and accompanying info within handler, defined by json file
+        :param input_dict: a MetaWorkflow step (object) and accompanying info within handler, defined by json file
         :type input_dict: dict
         """
         ### Basic (non-calculated) attributes ###
@@ -32,45 +34,38 @@ class MetaWorkflowStep(object):
         # Validate presence of basic attributes of this MetaWorkflow step
         self._validate_basic_attributes()
 
+        # Get rid of dependency duplicates -- TODO: already done within mwf?
+
+        # Initialize Metaworkflow (magma (ff?)) using embedded call to metaworkflow LinkTo
+        #TODO: do in magma ff?
+        # metaworkflow_linkto = getattr(self, "meta_workflow") #TODO: embedding API -- how to test??
+
+
         #TODO: import and call magma mwf to initialize the mwf within the handler
         # THEN check the dependencies
         # also need to fill in the names for the mwfs
 
-        ### Calculated attributes ###
-        # Nodes set, for building graph structure based on dependencies
-        self._nodes = set() #step_objects for steps that depend on current step
-        
-        # Names (strings) of MetaWorkflow steps that this MetaWorkflow is dependent on
-        if getattr(self, 'dependencies', None): # set None for [default] arg to not throw AttributeError
-            self.dependencies = set(self.dependencies) # convert to set to not have duplicates
-        else:
-            self.dependencies = set()
+        #TODO: check that names of metaworkflow steps are unique -- also
+        # use setdefault for filling in names (in ff? or here?) -- rather, check circularity
 
         #TODO: case where a metaworkflow is repeated downstream? does this ever happen?
 
 
-    def _validate_basic_attributes(self):
+    def _validate_basic_attributes(self): #TODO: create this as part of the utility function?
         """
         Validation of the JSON input for the MetaWorkflow step
+
         Checks that necessary MetaWorkflow attributes are present for this MetaWorkflow step
         """
+        check_presence_of_attributes(self, ["meta_workflow", "name", "duplication_flag"])
+        # str, must be unique TODO: name filling in ff
         try:
-            getattr(self, "meta_workflow") #str, must not be unique TODO: check this
-            getattr(self, "name") #str, must be unique TODO: name filling in ff
-            getattr(self, "duplication_flag") #bool
+            # set None for [default] arg to not throw AttributeError
+            if getattr(self, "items_for_creation_property_trace", True):
+                getattr(self, "items_for_creation_uuid")
         except AttributeError as e:
             raise ValueError("JSON validation error, {0}\n"
-                                .format(e.args[0]))
-
-        #TODO: is there another way to integrate this other than this weird nested try except
-        try:
-            getattr(self, "items_for_creation_property_trace")
-        except AttributeError:
-            try:
-                getattr(self, "items_for_creation_uuid")
-            except AttributeError as e:
-                raise ValueError("JSON validation error, {0}\n"
-                                .format(e.args[0]))
+                            .format(e.args[0]))
 
 
 
@@ -87,45 +82,45 @@ class MetaWorkflowHandler(object):
         """
         Constructor method, initialize object and attributes.
 
-        :param input_dict: MetaWorkflow Handler object dictionary, defined by json file
+        :param input_dict: MetaWorkflow Handler object, defined by json file from portal
         :type input_dict: dict
         """
 
         ### Basic attributes ###
-        # Required: project, institution
-        # Identifying: uuid, aliases, accession
-        # Commonly present: title, name, description, meta_workflows (list)
-        # see cgap_portal meta_workflow_handler schema for more info
+
         for key in input_dict:
             setattr(self, key, input_dict[key])
 
         # Validate presence of basic attributes of this MetaWorkflow Handler
-        self._validate_basic_attributes()
+        # - Required: project, institution TODO: taken care of in schema
+        # - Identifying: uuid, aliases, accession TODO: is this kinda the same as required?
+        # - Commonly present: title, name, description, meta_workflows (list)
+        # See cgap_portal meta_workflow_handler schema for more info.
+        check_presence_of_attributes(self, ["uuid"])
 
-        # check for meta_workflows attribute, set empty if it is not present
-        if getattr(self, 'meta_workflows', None): # set None for [default] arg to not throw AttributeError
-            self.meta_workflows = self.meta_workflows # convert to set to not have duplicates
-        else:
-            self.meta_workflows = []
+        ### Calculated attributes ###
 
-#         # Calculated attributes
-#         self.steps = {} #{step_obj.name: step_obj, ...}
-#         self._end_workflows = None
+        # to check for non-existent meta_workflows attribute
+        self._set_meta_workflows_list()
 
-#         # Calculate attributes
-#         self._validate_basic_attributes()
-#         self._read_steps()
+        # order the meta_workflows list based on dependencies
+        ordered_meta_workflows = generate_ordered_steps_list(self.meta_workflows, "name", "dependencies")
+        self.meta_workflows = ordered_meta_workflows
 
+        # create MetaWorkflow object for each metaworkflow step in meta_workflows
+        #TODO: do in magma-ff? because requires pulling metadata using UUID
+        #self.create_meta_workflow_steps()
 
-    def _validate_basic_attributes(self):
+    def _set_meta_workflows_list(self):
         """
+        Checks for meta_workflows attribute, gets rid of duplicates,
+        else sets as empty list if not present
+        TODO: better to throw error if duplicates are present?
         """
-        try:
-            getattr(self, 'uuid') #str, must be unique
-            getattr(self, 'input') #list
-            # getattr(self, 'meta_workflows') #list -- TODO: what if it's empty? -- took care of that in init
-            #TODO: check project and institution? I think the schema takes care of that
-        except AttributeError as e:
-            raise ValueError('JSON validation error, {0}\n'
-                                .format(e.args[0]))
+        set_list_attributes(self, ["meta_workflows"])
 
+    # def create_meta_workflow_steps(self): #TODO: in magma ff?
+    #     meta_workflows_list = getattr(self, "meta_workflows") # list
+    #     for meta_workflow_step in meta_workflows_list:
+
+    #TODO: getting global input of first step ## getattr(self, 'input') # list
