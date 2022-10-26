81c826d47db3b61ed496ee58d64ddfceb02d6c9e
vstevensf <victoria_stevens@hms.harvard.edu>
2022-10-26 17:56:43 -0400

    Added pytests for magma/utils.py

--- test/test_utils_magma.py
+++ test/test_utils_magma.py
@@ -3,7 +3,7 @@
 #################################################################
 import pytest
 
-from magma.utils import check_list_is_all_strings, check_presence_of_attributes, set_list_attributes
+from magma.utils import check_variable_type, check_list_elements_type
 
 #################################################################
 #   Vars
@@ -13,56 +13,123 @@ from magma.utils import check_list_is_all_strings, check_presence_of_attributes,
 #   Tests
 #################################################################
 
-#TODO: replace all check_list_is_all_strings tests accordingly
+class TestCheckVariableType:
+    @pytest.mark.parametrize(
+        "variable, intended_type",
+        [
+            (2, int),
+            (-2, int),
+            (float('inf'), float),
+            (complex(1, 1.0), complex),
+            (True, bool),
+            (False, bool),
+            (None, type(None)),
+            (None, object),
+            ('a', str),
+            ('a', object),
+            ("test", str),
+            ("test", object),
+            ((1, 2), tuple),
+            ((1, 2), object),
+            ([], list),
+            ([], object),
+            (set(), set),
+            (set(), object),
+            ([1, "test"], list),
+            ([1, "test"], object),
+            ({}, dict),
+            ({}, object),
+            ({"hi": 1}, dict),
+            ({"hi": 1}, object)
+        ]
+    )
+    def test_check_list_elements_type_no_errors(self, variable, intended_type):
+        """
+        Test for function checking if a variable is of a specified type,
+        no errors raised.
+        """
+        result = check_variable_type(variable, intended_type)
+        assert result == None
 
-@pytest.mark.parametrize(
-    "list_to_check,expected",
-    [
-        ([], None),
-        (["id"], None),
-        (["1", "test", "2"], None)
-    ],
-)
-def test_check_list_is_all_strings_no_errors(list_to_check, expected):
-    """
-    Test for function checking if all elements of a list are strings,
-    no errors raised
-    """
-    result = check_list_is_all_strings(list_to_check)
-    assert result == expected
+    @pytest.mark.parametrize(
+        "variable, intended_type",
+        [
+            (2, list),
+            (float('inf'), int),
+            (complex(1, 1.0), float),
+            (True, str),
+            (None, bool),
+            ('a', int),
+            ("test", list),
+            ((1, 2), set),
+            (set(), tuple),
+            ([1, "test"], dict),
+            ({"hi": 1}, list)
+        ]
+    )
+    def test_check_variable_type_typeerror(self, variable, intended_type):
+        """
+        Test for function checking if a variable is of a specified type,
+        TypeError raised.
+        """
+        with pytest.raises(TypeError) as type_err_info:
+            check_variable_type(variable, intended_type)
+        assert str(type_err_info.value) == "Input must be of type {0}".format(str(intended_type))
 
-@pytest.mark.parametrize(
-    "list_to_check",
-    [
-        ([["1", "2", "3", "4", "5"], ["6"]]),
-        ([["1", "2", "3", "4", "5"], "6"]),
-        ([None, "test"]),
-        ([1, "test"])
-    ],
-)
-def test_check_list_is_all_strings_with_exceptions(list_to_check):
-    """
-    Test for function checking if all elements of a list are strings,
-    Exception raised
-    """
-    with pytest.raises(Exception) as excinfo:
-        check_list_is_all_strings(list_to_check)
-    assert str(excinfo.value) == "All elements in list must be strings"
+class TestListElementsType:
+    @pytest.mark.parametrize(
+        "list_to_check, intended_type",
+        [
+            ([], str),
+            ([], int),
+            ([], list),
+            ([], object),
+            (["id"], str),
+            (["1", "test", "2"], str),
+            ([1, 2, 3, 4], int),
+            ([[1], [2], ["test", "2"], []], list)
+        ]
+    )
+    def test_check_list_elements_type_no_errors(self, list_to_check, intended_type):
+        """
+        Test for function checking that all elements of a list are of a specified type,
+        no errors raised.
+        """
+        result = check_list_elements_type(list_to_check, intended_type)
+        assert result == None
 
-@pytest.mark.parametrize(
-    "list_to_check",
-    [
-        (),
-        (None),
-        (1),
-        ("test"),
-        (set())
-    ],
-)
-def test_check_list_is_all_strings_with_type_error(list_to_check):
-    """
-    Test for function checking if all elements of a list are strings,
-    TypeError raised (incorrect argument type passed)
-    """
-    with pytest.raises(TypeError):
-        check_list_is_all_strings(list_to_check)
+    @pytest.mark.parametrize(
+        "list_to_check",
+        [
+            ((["1", "2", "3", "4", "5"], ["6"])),
+            (None),
+            ("test")
+        ]
+    )
+    def test_check_list_elements_type_listtocheck_not_list(self, list_to_check):
+        """
+        Test for function checking if all elements of a list are strings,
+        TypeError raised (list_to_check not a list)
+        """
+        with pytest.raises(TypeError) as type_err_info:
+            check_list_elements_type(list_to_check, str)
+        assert str(type_err_info.value) == "Input must be of type {0}".format(str(list))
+
+
+    @pytest.mark.parametrize(
+        "list_to_check, intended_type",
+        [
+            ([["1", "2", "3", "4", "5"], ["6"]], str),
+            ([["1", "2", "3", "4", "5"], "6"], list),
+            ([None, "test"], str),
+            ([1, "test"], int)
+        ]
+    )
+    def test_check_list_elements_type_typeerror(self, list_to_check, intended_type):
+        """
+        Test for function checking if all elements of a list are strings,
+        Exception raised
+        """
+        with pytest.raises(TypeError) as type_err_info:
+            check_list_elements_type(list_to_check, intended_type)
+        assert str(type_err_info.value) == "All elements in list must be of type {0}".format(str(intended_type))
