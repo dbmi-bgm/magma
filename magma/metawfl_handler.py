87952865a0aff2518892c14a03005d12cbe782e6
vstevensf <victoria_stevens@hms.harvard.edu>
2022-11-28 14:59:24 -0500

    Finished validation of MWF handler and its corresponding pytests.

--- magma/metawfl_handler.py
+++ magma/metawfl_handler.py
@@ -8,21 +8,52 @@
 ################################################
 #   TODO: functions for dcic utils -- move later
 ################################################
-from magma.utils import check_presence_of_attributes, set_list_attributes
+from magma.utils import check_presence_of_attributes, set_unique_list_attributes
 from magma.topological_sort import generate_ordered_steps_list
 
+################################################
+#   ValidatedDictionary
+################################################
+class ValidatedDictionary(object):
+    """
+    Parent class for MetaWorkflowStep and MetaWorkflowHandler classes.
+    Takes in an input dictionary, and validates basic attributes.
+    """
 
-#TODO: make parent class maybe
+    def __init__(self, input_dict):
+        """
+        Constructor method, initialize object and attributes.
+
+        :param input_dict: input dictionary, defined by json file, which defines basic attributes of this object
+        :type input_dict: dict
+        """
+        # Set basic (non-calculated) attributes #
+        for key in input_dict:
+            setattr(self, key, input_dict[key])
+
+    def _validate_basic_attributes(self, list_of_attributes=None):
+        """
+        Validation of the JSON input for this object.
+        Checks that given attributes are present in the created object.
+        """
+        check_presence_of_attributes(self, list_of_attributes)
 
 ################################################
 #   MetaWorkflowStep
 ################################################
 
-class MetaWorkflowStep(object):
+class MetaWorkflowStep(ValidatedDictionary):
     """
     Class to represent a MetaWorkflow object,
     as a step within a MetaWorkflow Handler object
     """
+
+    META_WORKFLOW_ATTR = "meta_workflow"
+    NAME_ATTR = "name"
+    DUP_FLAG_ATTR = "duplication_flag"
+    ITEMS_CREATION_PROP_TRACE = "items_for_creation_property_trace"
+    ITEMS_CREATION_UUID = "items_for_creation_uuid"
+    LIST_OF_ATTRS = [META_WORKFLOW_ATTR, NAME_ATTR, DUP_FLAG_ATTR]
 
     def __init__(self, input_dict):
         """
@@ -31,42 +62,24 @@ class MetaWorkflowStep(object):
         :param input_dict: a MetaWorkflow step (object) and accompanying info within handler, defined by json file
         :type input_dict: dict
         """
-        ### Basic (non-calculated) attributes ###
-        for key in input_dict:
-            setattr(self, key, input_dict[key])
+        super().__init__(input_dict)
 
         # Validate presence of basic attributes of this MetaWorkflow step
-        self._validate_basic_attributes()
+        self._validate_basic_attributes(self.LIST_OF_ATTRS)
 
-        # Get rid of dependency duplicates -- TODO: already done within mwf?
-
-        # Initialize Metaworkflow (magma (ff?)) using embedded call to metaworkflow LinkTo
-        #TODO: do in magma ff?
-        # metaworkflow_linkto = getattr(self, "meta_workflow") #TODO: embedding API -- how to test??
-
-
-        #TODO: import and call magma mwf to initialize the mwf within the handler
-        # THEN check the dependencies
-        # also need to fill in the names for the mwfs
-
-        #TODO: check that names of metaworkflow steps are unique -- also
-        # use setdefault for filling in names (in ff? or here?) -- rather, check circularity
-
-        #TODO: case where a metaworkflow is repeated downstream? does this ever happen?
-
-
-    def _validate_basic_attributes(self): #TODO: create this as part of the utility function?
+    def _validate_basic_attributes(self, list_of_attributes):
         """
-        Validation of the JSON input for the MetaWorkflow step
-
-        Checks that necessary MetaWorkflow attributes are present for this MetaWorkflow step
+        Validation of the JSON input for the MetaWorkflow step.
+        Checks that necessary MetaWorkflow attributes are present for this MetaWorkflow step.
         """
-        check_presence_of_attributes(self, ["meta_workflow", "name", "duplication_flag"])
+        super()._validate_basic_attributes(list_of_attributes)
         # str, must be unique TODO: name filling in ff
         try:
+            #TODO: what about if both are present? UUID is taken as default for now
             # set None for [default] arg to not throw AttributeError
-            if getattr(self, "items_for_creation_property_trace", True):
-                getattr(self, "items_for_creation_uuid")
+            if not getattr(self, self.ITEMS_CREATION_UUID, None):
+                # import pdb; pdb.set_trace()
+                getattr(self, self.ITEMS_CREATION_PROP_TRACE)
         except AttributeError as e:
             raise ValueError("JSON validation error, {0}\n"
                             .format(e.args[0]))
@@ -76,11 +89,17 @@ class MetaWorkflowStep(object):
 ################################################
 #   MetaWorkflowHandler
 ################################################
-class MetaWorkflowHandler(object):
+class MetaWorkflowHandler(ValidatedDictionary):
     """
     Class representing a MetaWorkflow Handler object,
     a list of MetaWorkflows with specified dependencies
     """
+
+    UUID_ATTR = "uuid"
+    META_WORKFLOW_NAME_ATTR = "name"
+    META_WORKFLOW_DEPENDENCIES_ATTR = "dependencies"
+    META_WORKFLOWS_ATTR = "meta_workflows"
+    LIST_OF_ATTRS = [UUID_ATTR]
 
     def __init__(self, input_dict):
         """
@@ -89,43 +108,36 @@ class MetaWorkflowHandler(object):
         :param input_dict: MetaWorkflow Handler object, defined by json file from portal
         :type input_dict: dict
         """
-
         ### Basic attributes ###
-
-        for key in input_dict:
-            setattr(self, key, input_dict[key])
-
-        # Validate presence of basic attributes of this MetaWorkflow Handler
-        # - Required: project, institution TODO: taken care of in schema
-        # - Identifying: uuid, aliases, accession TODO: is this kinda the same as required?
-        # - Commonly present: title, name, description, meta_workflows (list)
-        # See cgap_portal meta_workflow_handler schema for more info.
-        check_presence_of_attributes(self, ["uuid"])
+        super().__init__(input_dict)
+        
+        super()._validate_basic_attributes(self.LIST_OF_ATTRS)
 
         ### Calculated attributes ###
-
         # to check for non-existent meta_workflows attribute
         self._set_meta_workflows_list()
 
         # order the meta_workflows list based on dependencies
-        ordered_meta_workflows = generate_ordered_steps_list(self.meta_workflows, "name", "dependencies")
-        self.ordered_meta_workflows = ordered_meta_workflows
-        #TODO: should i make this a new calculated attribute, rather than redefining? YES
+        # ordered_meta_workflows = generate_ordered_steps_list(self.meta_workflows, self.META_WORKFLOW_NAME_ATTR, self.META_WORKFLOW_DEPENDENCIES_ATTR)
+        self.ordered_meta_workflows = self._create_ordered_meta_workflows_list()
 
-        # create MetaWorkflow object for each metaworkflow step in meta_workflows
-        #TODO: do in magma-ff? because requires pulling metadata using UUID
-        #self.create_meta_workflow_steps()
+        # using ordered metaworkflows list, create a list of objects using class MetaWorkflowStep
+        # this validates basic attributes needed for each metaworkflow step
+        self.ordered_meta_workflow_steps = self._create_meta_workflow_step_objects()
 
     def _set_meta_workflows_list(self):
         """
         Checks for meta_workflows attribute, gets rid of duplicates,
         else sets as empty list if not present
-        TODO: better to throw error if duplicates are present?
         """
-        set_list_attributes(self, ["meta_workflows"])
+        set_unique_list_attributes(self, [self.META_WORKFLOWS_ATTR])
 
-    # def create_meta_workflow_steps(self): #TODO: in magma ff?
-    #     meta_workflows_list = getattr(self, "meta_workflows") # list
-    #     for meta_workflow_step in meta_workflows_list:
+    def _create_ordered_meta_workflows_list(self):
+        return generate_ordered_steps_list(self.meta_workflows, self.META_WORKFLOW_NAME_ATTR, self.META_WORKFLOW_DEPENDENCIES_ATTR)
 
-    #TODO: getting global input of first step ## getattr(self, 'input') # list
+    def _create_meta_workflow_step_objects(self):
+        meta_workflow_step_list = []
+        for meta_workflow in self.ordered_meta_workflows:
+            meta_workflow_step_object = MetaWorkflowStep(meta_workflow)
+            meta_workflow_step_list.append(meta_workflow_step_object)
+        return meta_workflow_step_list
--- test/test_metawfl_handler.py
+++ test/test_metawfl_handler.py
@@ -2,16 +2,240 @@
 #   Libraries
 #################################################################
 import pytest
-import json
+from copy import deepcopy
 
-from magma import metawfl_handler as mwfh
+from magma.metawfl_handler import MetaWorkflowStep, MetaWorkflowHandler
 
-#TODO: how is the json object passed to magma? as list? or dict?
+#################################################################
+#   Vars
+#################################################################
 
-with open('test/files/test_METAWFL_HANDLER.json') as json_file:
-    data = json.load(json_file)
+MWF_HANDLER_NAME = "test_mwf_handler"
+PROJECT = "test_project"
+INSTITUTION = "test_institution"
+MWF_HANDLER_UUID = "test_mwf_handler_uuid"
 
-print(data)
-print(type(data))
+TESTER_UUID = "test_item_uuid"
 
-mwfh.MetaWorkflowHandler(data)
+# basic meta_workflow steps (dicts) used in meta_workflows array
+#TODO: for validation of basic attributes, what if the value of an attribute is None?
+# e.g. name or meta_workflow in metaworkflowstep? (because my helper function
+# only checks that you can get the actual attribute, but getattr works still
+# if the value is None)
+MWF_A = {"meta_workflow": "test_mwf_uuid_0", "name": "A"}
+MWF_B = {"meta_workflow": "test_mwf_uuid_1", "name": "B"}
+MWF_C = {"meta_workflow": "test_mwf_uuid_2", "name": "C"}
+MWF_D = {"meta_workflow": "test_mwf_uuid_3", "name": "D"}
+
+DEP_ON_A = ["A"]
+DEP_ON_B = ["B"]
+DEP_ON_C = ["C"]
+DEP_ON_D = ["D"]
+
+def mwf_with_added_attrs(metaworkflow_dict, items_for_creation_property_trace=None, items_for_creation_uuid=None, dependencies=None, duplication_flag=None):
+    dict_copy = deepcopy(metaworkflow_dict)
+    if items_for_creation_property_trace:
+        dict_copy["items_for_creation_property_trace"] = items_for_creation_property_trace
+    if items_for_creation_uuid:
+        dict_copy["items_for_creation_uuid"] = items_for_creation_uuid
+    if dependencies is not None:
+        dict_copy["dependencies"] = dependencies
+    if duplication_flag is not None:
+        dict_copy["duplication_flag"] = duplication_flag
+    return dict_copy
+
+
+
+# meta_workflows arrays for MetaWorkflow Handler
+# handler without uuid -- fails validation of basic attributes
+HANDLER_WITHOUT_UUID = {
+    "name": MWF_HANDLER_NAME,
+    "project": PROJECT,
+    "institution": INSTITUTION
+}
+
+# handler without metaworkflows array -- passes validation, should set empty metaworkflows array
+HANDLER_WITHOUT_MWF_ARRAY = {
+    "name": MWF_HANDLER_NAME,
+    "project": PROJECT,
+    "institution": INSTITUTION,
+    "uuid": MWF_HANDLER_UUID
+}
+
+# DAG_0
+# A     B -----> C
+MWF_A_DAG_0 = mwf_with_added_attrs(MWF_A, None, TESTER_UUID, [], True)
+MWF_B_DAG_0 = mwf_with_added_attrs(MWF_B, None, TESTER_UUID, [], True)
+MWF_C_DAG_0 = mwf_with_added_attrs(MWF_C, None, TESTER_UUID, DEP_ON_B, True)
+DAG_0_MWF_ARRAY = [MWF_B_DAG_0, MWF_A_DAG_0, MWF_C_DAG_0] # purposely in this order to test toposort
+HANDLER_DAG_0 = {
+    "name": MWF_HANDLER_NAME,
+    "project": PROJECT,
+    "institution": INSTITUTION,
+    "uuid": MWF_HANDLER_UUID,
+    "meta_workflows": DAG_0_MWF_ARRAY
+}
+DAG_0_MWF_ARRAY_W_DUPLICATES = [MWF_B_DAG_0, MWF_A_DAG_0, MWF_C_DAG_0, MWF_B_DAG_0]
+HANDLER_DAG_0_W_DUPLICATES = {
+    "name": MWF_HANDLER_NAME,
+    "project": PROJECT,
+    "institution": INSTITUTION,
+    "uuid": MWF_HANDLER_UUID,
+    "meta_workflows": DAG_0_MWF_ARRAY_W_DUPLICATES
+}
+REORDERED_MWF_ARRAY_DAG_0 = [MWF_B_DAG_0, MWF_A_DAG_0, MWF_C_DAG_0]
+
+# DAG_1
+# B -----> D
+# |     ⋀  ⋀
+# |   /    |
+# ⋁ /      |
+# A <----- C 
+MWF_A_DAG_1 = mwf_with_added_attrs(MWF_A, None, TESTER_UUID, DEP_ON_B + DEP_ON_C, True)
+MWF_B_DAG_1 = mwf_with_added_attrs(MWF_B, None, TESTER_UUID, [], True)
+MWF_C_DAG_1 = mwf_with_added_attrs(MWF_C, None, TESTER_UUID, [], True)
+MWF_D_DAG_1 = mwf_with_added_attrs(MWF_D, None, TESTER_UUID, DEP_ON_A + DEP_ON_B + DEP_ON_C, True)
+DAG_1_MWF_ARRAY = [MWF_A_DAG_1, MWF_B_DAG_1, MWF_C_DAG_1, MWF_D_DAG_1]
+HANDLER_DAG_1 = {
+    "name": MWF_HANDLER_NAME,
+    "project": PROJECT,
+    "institution": INSTITUTION,
+    "uuid": MWF_HANDLER_UUID,
+    "meta_workflows": DAG_1_MWF_ARRAY
+}
+MWF_B_DAG_1_W_SELF_DEP = mwf_with_added_attrs(MWF_B, None, TESTER_UUID, DEP_ON_B, True)
+DAG_1_MWF_ARRAY_W_SELF_DEP = [MWF_A_DAG_1, MWF_B_DAG_1_W_SELF_DEP, MWF_C_DAG_1, MWF_D_DAG_1]
+HANDLER_DAG_1_W_SELF_DEP = {
+    "name": MWF_HANDLER_NAME,
+    "project": PROJECT,
+    "institution": INSTITUTION,
+    "uuid": MWF_HANDLER_UUID,
+    "meta_workflows": DAG_1_MWF_ARRAY_W_SELF_DEP
+}
+REORDERED_MWF_ARRAY_DAG_1 = [MWF_B_DAG_1, MWF_C_DAG_1, MWF_A_DAG_1, MWF_D_DAG_1]
+
+# CYCLIC_0
+# A        B__
+#          ⋀  \_____ 
+#          |        |
+#          |        |
+#          C <----- | 
+MWF_A_CYCLIC_0 = mwf_with_added_attrs(MWF_A, None, TESTER_UUID, [], True)
+MWF_B_CYCLIC_0 = mwf_with_added_attrs(MWF_B, None, TESTER_UUID, DEP_ON_C, True)
+MWF_C_CYCLIC_0 = mwf_with_added_attrs(MWF_C, None, TESTER_UUID, DEP_ON_B, True)
+CYCLIC_0_MWF_ARRAY = [MWF_A_CYCLIC_0, MWF_B_CYCLIC_0, MWF_C_CYCLIC_0]
+HANDLER_CYCLIC_0 = {
+    "name": MWF_HANDLER_NAME,
+    "project": PROJECT,
+    "institution": INSTITUTION,
+    "uuid": MWF_HANDLER_UUID,
+    "meta_workflows": CYCLIC_0_MWF_ARRAY
+}
+
+# CYCLIC_1
+# A -----> B
+# ⋀        |
+# |        |
+# |        ⋁
+# D <----- C 
+MWF_A_CYCLIC_1 = mwf_with_added_attrs(MWF_A, None, TESTER_UUID, DEP_ON_D, True)
+MWF_B_CYCLIC_1 = mwf_with_added_attrs(MWF_B, None, TESTER_UUID, DEP_ON_A, True)
+MWF_C_CYCLIC_1 = mwf_with_added_attrs(MWF_C, None, TESTER_UUID, DEP_ON_B, True)
+MWF_D_CYCLIC_1 = mwf_with_added_attrs(MWF_D, None, TESTER_UUID, DEP_ON_C, True)
+CYCLIC_1_MWF_ARRAY = [MWF_A_CYCLIC_1, MWF_B_CYCLIC_1, MWF_C_CYCLIC_1, MWF_D_CYCLIC_1]
+HANDLER_CYCLIC_1 = {
+    "name": MWF_HANDLER_NAME,
+    "project": PROJECT,
+    "institution": INSTITUTION,
+    "uuid": MWF_HANDLER_UUID,
+    "meta_workflows": CYCLIC_1_MWF_ARRAY
+}
+
+#################################################################
+#   Tests
+#################################################################
+
+#TODO: make test for ValidatedDictionary parent object?
+# I basically test it through the child classes below
+
+@pytest.mark.parametrize(
+    "mwf_step_dict, items_for_creation_property_trace, items_for_creation_uuid, dependencies, duplication_flag, num_attributes",
+    [
+        (MWF_A, "sample_processing.samples", None, None, True, 4),
+        (MWF_B, None, TESTER_UUID, None, False, 4),
+        (MWF_B, None, TESTER_UUID, DEP_ON_A, True, 5),
+        (MWF_C, "sample_processing.samples", TESTER_UUID, None, True, 5), # items for creation UUID taken by default
+        # the following should throw ValueError
+        (MWF_A, None, None, None, True, None), # missing items for creation
+        (MWF_A, None, TESTER_UUID, None, None, None) # missing duplication flag
+    ]
+)
+def test_attribute_validation_mwf_step(mwf_step_dict, items_for_creation_property_trace, items_for_creation_uuid, dependencies, duplication_flag, num_attributes):
+    try:
+        completed_dict = mwf_with_added_attrs(mwf_step_dict, items_for_creation_property_trace, items_for_creation_uuid, dependencies, duplication_flag)
+        meta_workflow_step_object = MetaWorkflowStep(completed_dict)
+    except ValueError as val_err_info:
+        assert "validation error" in str(val_err_info)
+    else:
+        assert num_attributes == len(meta_workflow_step_object.__dict__)
+
+
+class TestMetaWorkflowHandler:
+    def test_attribute_validation_mwf_handler(self):
+        with pytest.raises(ValueError) as val_err_info:
+            meta_workflow_handler = MetaWorkflowHandler(HANDLER_WITHOUT_UUID)
+            assert "validation error" in str(val_err_info)
+
+    @pytest.mark.parametrize(
+        "mwf_handler_dict, length_of_mwf_list",
+        [
+            (HANDLER_WITHOUT_MWF_ARRAY, 0), # sets empty list if attr not present
+            (HANDLER_DAG_0, 3),
+            (HANDLER_DAG_0_W_DUPLICATES, 3) # gets rid of duplicates
+        ]
+    )
+    def test_set_meta_workflows_list(self, mwf_handler_dict, length_of_mwf_list):
+        meta_workflow_handler = MetaWorkflowHandler(mwf_handler_dict)
+        assert len(getattr(meta_workflow_handler, "meta_workflows")) == length_of_mwf_list
+
+    @pytest.mark.parametrize(
+        "mwf_handler_dict, reordered_mwf_list",
+        [
+            (HANDLER_WITHOUT_MWF_ARRAY, []),
+            (HANDLER_DAG_0, REORDERED_MWF_ARRAY_DAG_0),
+            (HANDLER_DAG_0_W_DUPLICATES, REORDERED_MWF_ARRAY_DAG_0),
+            (HANDLER_DAG_1, REORDERED_MWF_ARRAY_DAG_1),
+            (HANDLER_DAG_1_W_SELF_DEP, REORDERED_MWF_ARRAY_DAG_1)
+        ]
+    )
+    def test_create_ordered_meta_workflows_list(self, mwf_handler_dict, reordered_mwf_list):
+        meta_workflow_handler = MetaWorkflowHandler(mwf_handler_dict)
+        assert getattr(meta_workflow_handler, "ordered_meta_workflows") == reordered_mwf_list
+
+    @pytest.mark.parametrize(
+        "mwf_handler_dict",
+        [
+            (HANDLER_CYCLIC_0),
+            (HANDLER_CYCLIC_1)
+        ]
+    )
+    def test_cycles(self, mwf_handler_dict):
+        with pytest.raises(Exception) as exc_info:
+            meta_workflow_handler = MetaWorkflowHandler(mwf_handler_dict)
+            assert "Cycle in graph: node" in str(exc_info)
+
+    @pytest.mark.parametrize(
+        "mwf_handler_dict",
+        [
+            (HANDLER_WITHOUT_MWF_ARRAY),
+            (HANDLER_DAG_0),
+            (HANDLER_DAG_0_W_DUPLICATES),
+            (HANDLER_DAG_1),
+            (HANDLER_DAG_1_W_SELF_DEP)
+        ]
+    )
+    def test_create_ordered_meta_workflow_steps_list(self, mwf_handler_dict):
+        meta_workflow_handler = MetaWorkflowHandler(mwf_handler_dict)
+        ordered_meta_workflow_steps = getattr(meta_workflow_handler, "ordered_meta_workflow_steps")
+        for step in ordered_meta_workflow_steps:
+            assert isinstance(step, MetaWorkflowStep)
